117d5c2e65084b87b884d94db6b00caf7b051838
Requelfade <39383721+Requelfade@users.noreply.github.com>
2018-10-19 13:35:54 +0200

    XD

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 ##pip install colorama
-
+import json
+from pprint import pprint
 import urllib3
 urllib3.disable_warnings()
 
@@ -13,30 +14,35 @@ Fore: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
 Back: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
 Style: DIM, NORMAL, BRIGHT, RESET_ALL
 """
+lolpath="E:\3a\programy\"
 
 headers = {
     'Accept' : 'application/json',
-    'Authorization' : 'Basic cmlvdDpmUTBEVzVDWUE0a0d2eFZnSEVqYzB3'
+    'Authorization' : 'Basic cmlvdDpnV0puTDVya0ZEWFB2cWlydjBCMUl3'
 }
 
 def rqget(url, headers):
-    url='https://127.0.0.1:51988'+url
+    url='https://127.0.0.1:65119/'+url
     return requests.get(url, headers=headers, verify=False)
 def rqpost(url, headers):
-    url='https://127.0.0.1:51988'+url
+    url='https://127.0.0.1:65119/'+url
     return requests.post(url, headers=headers, verify=False)
 
 def CheckLauncherScale():
-    LauncherScale = rqget('/riotclient/zoom-scale', headers)
+    LauncherScale = rqget('riotclient/zoom-scale', headers)
     LauncherScale.json
     if LauncherScale.text == '1.25':
         return Fore.GREEN + 'OK'
     else:
-        rqpost('/riotclient/zoom-scale?newZoomScale=1.25', headers)
+        rqpost('riotclient/zoom-scale?newZoomScale=1.25', headers)
         return Fore.YELLOW + 'Fixed'
         
 
 
 print('Launcher Size: ['+ CheckLauncherScale() + Fore.RESET + ']')
 
-#print(CheckLauncherScale())
+print(CheckLauncherScale())
+
+data = rqget("lol-collections/v1/inventories/chest-eligibility", headers).text
+parsed_json = json.loads(data)
+print(parsed_json["earnableChests"])
